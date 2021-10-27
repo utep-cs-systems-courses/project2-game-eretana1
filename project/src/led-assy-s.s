d91e95528c6d5b90aeef1908ed55e06f8c7afece
Efrain Retana <eretana1@miners.utep.edu>
2021-10-27 10:53:45 -0600

    Fix issue in assembly code where green LED would not turn on.

--- project/src/led-assy-s.s
+++ project/src/led-assy-s.s
@@ -7,22 +7,22 @@
 
 alternate_leds:
 	cmp #500, r12
-	jnc red_on
+	jnc red_on		
 	bis.b #64, &P1OUT
-	bis.b #0, &P1OUT
+	bis.b #1, &P1OUT
 	pop r0
 
 red_on:
 	cmp #250, r12
 	jnc green_on
 	bis.b #64 , &P1OUT
-	and.b #~0, &P1OUT
+	and.b #~1, &P1OUT
 	pop r0
 
 green_on:
 	cmp #125, r12
 	jnc exit
-	bis.b #0, &P1OUT
+	bis.b #1, &P1OUT
 	and.b #~64, &P1OUT
 	pop r0
 
